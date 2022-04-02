732ddbf02110a8968d64fa403f502d18ed6d6344
wanxuanh <wanxuan.ho@gmail.com>
2022-04-02 16:31:53 +0800

    delete

--- src/pages/HolidayList.jsx
+++ src/pages/HolidayList.jsx
@@ -6,17 +6,26 @@ function HolidayList() {
   const [holidays, setHolidays] = useState([]);
 
   useEffect(() => {
-    fetch(urlcat(BACKEND, "/api/holidays/seed"))
+    fetch(urlcat(BACKEND, "/api/holidays/"))
       .then((response) => response.json())
       .then((data) => setHolidays(data));
   }, []);
+
+  const handleDelete = (id) => () => {
+    const url = urlcat(BACKEND, `/api/holidays/${id}`);
+    fetch(url, { method: "DELETE" })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <>
       <ul>
         {holidays.map((holiday) => (
-          <li key={holiday.id}>
-            <a href="#">{holiday.name}</a>
+          <li key={holiday._id}>
+            <a href="/">{holiday.name}</a>
+            {" -- "}
+            <span onClick={handleDelete(holiday._id)}>Delete</span>
           </li>
         ))}
       </ul>
